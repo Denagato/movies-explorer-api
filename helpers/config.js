1749ff4a99fb8bb806e0216b41d391f0e8525ae2
Vasily Kurichev <dekill@yandex.ru>
2021-05-09 16:02:59 +0300

    cors

--- helpers/config.js
+++ helpers/config.js
@@ -1,7 +1,14 @@
 const BD = 'mongodb://localhost:27017/movies-explorer-db';
 const PORT_NUMBER = 3000;
 const CORS_OPTIONS = {
-  origin: ['*'],
+  origin: [
+    'http://denagato.movies.nomoredomains.icu',
+    'http://api.denagato.movies.nomoredomains.icu',
+    'https://denagato.movies.nomoredomains.icu',
+    'https://api.denagato.movies.nomoredomains.icu',
+    'http://localhost:3000',
+    'http://localhost:3001',
+  ],
   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
   preflightContinue: false,
   optionsSuccessStatus: 204,
